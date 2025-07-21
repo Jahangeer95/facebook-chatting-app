3ac80f09f1784f0a98fa83e16f8ad5f1e0339941
SadiaSher504 <124422751+SadiaSher504@users.noreply.github.com>
2025-07-21 15:12:52 +0500

    React setup for facebook integration

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1 className="text-3xl font-bold underline text-blue-500">
+          Hello Tailwind!
+        </h1>
+        <h2 className="text-yellow-300 text-5xl font-bold text-start ">
+          I am a student
+        </h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // or your actual path
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
