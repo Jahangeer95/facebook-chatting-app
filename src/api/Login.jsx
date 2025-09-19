9b804f51074818d75a3e6125d0401b3b6d1d7178
jahangeer ali <102212538+Jahangeer95@users.noreply.github.com>
2025-09-19 10:35:26 +0500

    Update Login.jsx added res in console for testing

--- src/api/Login.jsx
+++ src/api/Login.jsx
@@ -4,7 +4,7 @@ export const loginUser = async (email, password) => {
   try {
     const body = { email, password };
     const res = await Api.post(`user/login`, body);
-
+console.log({res})
     const token = res.headers['User_auth_token' || 'user_auth_token'];
     console.log("User Token:", token);
     console.log("User:", res.data);
