85a3e743966c98e5bdc05a1110f811cf142451a2
jahangeer ali <102212538+Jahangeer95@users.noreply.github.com>
2025-09-19 10:32:53 +0500

    Update Login.jsx testing 22

--- src/api/Login.jsx
+++ src/api/Login.jsx
@@ -5,7 +5,7 @@ export const loginUser = async (email, password) => {
     const body = { email, password };
     const res = await Api.post(`user/login`, body);
 
-    const token = res.headers['user_auth_token'];
+    const token = res.headers['User_auth_token' || 'user_auth_token'];
     console.log("User Token:", token);
     console.log("User:", res.data);
     return { data: res.data, token };
