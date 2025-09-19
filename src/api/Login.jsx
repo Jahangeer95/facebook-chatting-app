efbfb6a6fcbb335c4940e0f5c78ada16254edc0f
jahangeer ali <102212538+Jahangeer95@users.noreply.github.com>
2025-09-19 10:50:27 +0500

    Update Login.jsx res.status condition added."

--- src/api/Login.jsx
+++ src/api/Login.jsx
@@ -5,10 +5,13 @@ export const loginUser = async (email, password) => {
     const body = { email, password };
     const res = await Api.post(`user/login`, body);
 console.log({res})
+    if(res.status===200){
+      
     const token = res.headers['User_auth_token' || 'user_auth_token'];
     console.log("User Token:", token);
     console.log("User:", res.data);
     return { data: res.data, token };
+    }
   } catch (error) {
     console.error("Error :", error);
   }
