ff9a6c44b581aba831af9a4a83d5566bffa57e99
SadiaSher504 <124422751+SadiaSher504@users.noreply.github.com>
2025-10-06 21:57:32 +0500

    Add a back button at top left in header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,20 @@
-export function Header () {
-    return (
-      <div className="bg-blue-900 text-white p-2 text-2xl font-semibold text-center border-b border-gray-300 shadow-sm">
-         Facebook 
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
+
+export function Header() {
+  const navigate = useNavigate();
+  return (
+    <div className="bg-blue-900 text-white p-2 text-2xl font-semibold text-center border-b border-gray-300 shadow-sm">
+      <div className="flex items-center">
+        <button
+          className="hover:scale-105 w-fit text-lg px-2"
+          onClick={()=>navigate(`/pages`)}
+        >
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        <h1 className="flex-1 items-center">Facebook</h1>
       </div>
-    );
-  };
-  
+    </div>
+  );
+}
