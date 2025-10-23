cc7ad75e8a62633913098d553e0b411655b76554
SadiaSher504 <124422751+SadiaSher504@users.noreply.github.com>
2025-10-23 21:43:46 +0500

    Modify CampaignsList.jsx

--- src/components/campaigns/CampaignsList.jsx
+++ src/components/campaigns/CampaignsList.jsx
@@ -153,7 +153,7 @@ export function CampaignsList({
                       </td>
                       <td className="px-2 py-2 text-gray-600">{item.status}</td>
                       <td className="px-2 py-2 text-gray-600">
-                        {item.objective}
+                        {item.objective.replace("_"," ")}
                       </td>
                       <td className="px-2 py-2 text-gray-600">
                         {item.buying_type}
