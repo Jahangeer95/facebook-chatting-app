89e57cda0b2484b1c327cdce18c4a0af87339daa
SadiaSher504 <124422751+SadiaSher504@users.noreply.github.com>
2025-12-07 23:24:24 +0500

    Allow Owner to update the role of Admin

--- src/components/page/Users.jsx
+++ src/components/page/Users.jsx
@@ -74,7 +74,7 @@ export function Users({users,refreshUsers}) {
                   <td className="px-2 py-2 text-sm flex justify-between items-center">
                     <p className="w-[100px]">{item.role}</p>
                     {(user?.role === "ADMIN" || user?.role === "MANAGER"|| user?.role === "OWNER") &&
-                      (item.role !== "ADMIN" && item.role !== "OWNER") && (item._id !== user?._id) &&(
+                      ((item.role !== "ADMIN" && item.role !== "OWNER") || (user.role==="OWNER" && item.role!=="OWNER")) && (item._id !== user?._id) &&(
                         <select
                          value={selectedUser}
                           onChange={async (e) =>{
