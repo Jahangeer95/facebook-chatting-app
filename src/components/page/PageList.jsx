e0b7e7614776fff86b6986879cf756da05e6266c
SadiaSher504 <124422751+SadiaSher504@users.noreply.github.com>
2025-12-07 23:31:49 +0500

    Allow MANAGER to add user to page

--- src/components/page/PageList.jsx
+++ src/components/page/PageList.jsx
@@ -106,7 +106,7 @@ export function PageList({pages,fetchPage,refreshUsers,users}) {
                       View
                     </button>
                     {/* add users to page */}
-                    {(user?.role === "ADMIN" || user?.role === "OWNER" || user?.role === "Manager" ) && (
+                    {(user?.role === "ADMIN" || user?.role === "OWNER" || user?.role === "MANAGER" ) && (
                       <select
                         value={selectedUser}
                         onChange={async (e) =>{
